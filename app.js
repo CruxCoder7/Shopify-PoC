829af2797e7bd92410b705d466fb169830c4141e
joydip.dutta <joydip.dutta@theinternetfolks.com>
2023-05-22 17:05:24 +0530

    Did some route fixes

--- app.js
+++ app.js
@@ -8,12 +8,12 @@ const app = express()
 
 /**
  *  API endpoint to create a webhook
- *  Shopify will send the webhook event containing the payload to the `address` that we specifiy
+ *  Shopify will send the webhook event containing the payload to the `address` that we specify
  *  The `topic` is the action, by doing which, the webhook will be triggered. (Eg: products/update, customer/update, products/delete)
  *  The Access Token is the Shopify Store's access token
  */
-app.get("/createwebhk", async (req, res) => {
-    const resp = await fetch('https://tif-storee.myshopify.com/admin/api/2023-04/webhooks.json', {
+app.get("/create_webhook", async (req, res) => {
+    const resp = await fetch('https://2c746c.myshopify.com/admin/api/2023-04/webhooks.json', {
         method: 'POST',
         headers: {
             'X-Shopify-Access-Token': process.env.SHOPIFY_ACCESS_TOKEN,
@@ -21,7 +21,7 @@ app.get("/createwebhk", async (req, res) => {
         },
         body: JSON.stringify({
             'webhook': {
-                'address': 'https://ab1e-2406-7400-c6-b900-4953-c9a9-5b23-7a1e.ngrok-free.app/newhook',
+                'address': 'https://https://beta.client.dyrect.internetfolks.in/api/create_product',
                 'topic': 'products/create',
                 'format': 'json'
             }
@@ -33,13 +33,13 @@ app.get("/createwebhk", async (req, res) => {
 })
 
 /**
- * This is the endpoint of the `address` that we specified in the creation of the webhook. In this case, /newhook.
+ * This is the endpoint of the `address` that we specified in the creation of the webhook. In this case, /create_product.
  * In this case, whenever a product is created - The `topic` that we specified in the creation of the webhook, 
  * Shopify will send a hmacHeader. We have to generate a hash with this header and compare it with our Store's Secret Key.
  * The Payload sent by Shopify is in Buffer format. We have to convert it into JSON.
  * In this case, the payload will contain all the details about the product that we just created.
  */
-app.post('/newhook', express.raw({ type: 'application/json' }), (req, res) => {
+app.post('/create_product', express.raw({ type: 'application/json' }), (req, res) => {
     const hmacHeader = req.get('X-Shopify-Hmac-SHA256');
     const payload = req.body;
 
@@ -72,7 +72,7 @@ app.post('/newhook', express.raw({ type: 'application/json' }), (req, res) => {
  */
 app.get("/data", (req, res) => {
     // req.query.resource = products | webhooks
-    let url = `https://tif-storee.myshopify.com/admin/api/2023-04/${req.query.resource}.json`;
+    let url = `https://2c746c.myshopify.com/admin/api/2023-04/${req.query.resource}.json`;
     let options = {
         method: "GET",
         uri: url,
@@ -97,5 +97,5 @@ app.get("/", (req, res) => {
 })
 
 app.listen(3000, () => {
-    console.log("Listeing on PORT 3000");
+    console.log("Listening on PORT 3000");
 })
